f4a3c1f6dfe78a41a078a31519570a74daf574ae
Tymoteusz Gach <tymoteusz.gach@merapar.com>
2022-09-21 22:56:06 +0200

    chore: enable prettier

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,12 +1,15 @@
-import { cdk, javascript } from 'projen';
+import { cdk, javascript } from "projen";
+
 const project = new cdk.JsiiProject({
-  author: 'Tymoteusz Gach',
-  authorAddress: 'tymoteusz.gach@merapar.com',
-  defaultReleaseBranch: 'main',
-  name: 'projen-template',
+  author: "Tymoteusz Gach",
+  authorAddress: "tymoteusz.gach@merapar.com",
+  defaultReleaseBranch: "main",
+  name: "projen-template",
   packageManager: javascript.NodePackageManager.NPM,
   projenrcTs: true,
-  repositoryUrl: 'ssh://git@github.com/TymoteuszGach/projen-template.git',
-  gitignore: ['.idea'],
+  repositoryUrl: "ssh://git@github.com/TymoteuszGach/projen-template.git",
+  gitignore: [".idea"],
+  prettier: true,
 });
+
 project.synth();
