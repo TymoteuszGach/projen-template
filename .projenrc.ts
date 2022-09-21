4b6183d37c394be885033d5e2d9cab9d51e113b6
Tymoteusz Gach <tymoteusz.gach@merapar.com>
2022-09-21 22:46:18 +0200

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,12 @@
+import { cdk, javascript } from 'projen';
+const project = new cdk.JsiiProject({
+  author: 'Tymoteusz Gach',
+  authorAddress: 'tymoteusz.gach@merapar.com',
+  defaultReleaseBranch: 'main',
+  name: 'projen-template',
+  packageManager: javascript.NodePackageManager.NPM,
+  projenrcTs: true,
+  repositoryUrl: 'ssh://git@github.com/TymoteuszGach/projen-template.git',
+  gitignore: ['.idea'],
+});
+project.synth();
