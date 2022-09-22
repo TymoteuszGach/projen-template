7c0be496ed00fe322ea1622492d0a39662feac61
Tymoteusz Gach <tymoteusz.gach@merapar.com>
2022-09-22 09:00:38 +0200

    fix: bundle fs-extra dependencies

--- .projenrc.ts
+++ .projenrc.ts
@@ -18,9 +18,9 @@ const project = new cdk.JsiiProject({
   sampleCode: false,
   npmDistTag: "latest",
   npmRegistryUrl: "https://npm.pkg.github.com",
-  devDeps: ["fs-extra", "@types/fs-extra@^8"],
   deps: ["projen"],
   peerDeps: ["projen"],
+  bundledDeps: ["fs-extra", "@types/fs-extra@^8"],
 });
 
 project.synth();
